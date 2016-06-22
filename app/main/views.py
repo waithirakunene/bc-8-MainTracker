de04b58af7e7bca9dfd8d150094942f5d5313a1e
anne waithira <annewaithira28@gmail.com>
2016-06-22 09:00:09 +0300

    modified views added routes

--- app/main/views.py
+++ app/main/views.py
@@ -2,13 +2,30 @@ from flask import render_template, url_for, redirect, abort, flash, request, cur
 from flask_login import login_required, current_user
 from app import db
 from app.main import main
-from app.models import User, Facility, RepairPersons, Repairs
+from app.models import User, Facility, RepairPersons, Repairs, RepairStatus 
 
 from app.main.forms import (
    AddFacilityDetailsForm, AddRepairPersons, RepairDetailsForm, RequestRepairForm
 )
 
-@main.route('/add_facility', methods=['GET','POST'])
+@main.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    tasks = []
+    if current_user.is_admin:
+        repairs = Repairs.query.order_by(Repairs.date_requested.desc()).all()
+        pending = len([repair for repair in repairs if not repairs.resolved])
+        form = AddFacilityDetailsForm()
+        return render_template('main/add_facility.html', form=form)
+    else:
+        repairs = Repairs.query.filter_by(
+            requested_by_id=current_user.id).order_by(Repairs.date_requested.desc())
+        pending = len([repair for repair in repairs if not repair.resolved])
+        form = RequestRepairForm()
+        return render_template('main/request_repair.html', form=form)
+
+
+@main.route('/add-facility', methods=['GET','POST'])
 def add_facility():
     form = AddFacilityDetailsForm()
     if form.validate_on_submit():
@@ -28,7 +45,7 @@ def add_facility():
         
 @main.route('/add_repair_persons', methods=['GET','POST'])
 @login_required
-def add_repair_persons():
+def add_maintainer():
     form = AddRepairPersons()
     if form.validate_on_submit():
         repairPerson = RepairPersons( 
@@ -40,8 +57,11 @@ def add_repair_persons():
         db.session.add(repairPerson)
         db.session.commit()
         flash('You have added one person')
-        return redirect(url_for('main.add_repair_persons'))
-    return render_template('main/add_repair_persons.html', form=form)
+        return redirect(url_for('main.add_maintainer'))
+    return render_template('main/add_maintainer.html', form=form)
+
+
+
 
 @main.route('/request-repair', methods=['GET', 'POST'])
 @login_required
@@ -62,9 +82,26 @@ def request_repair():
 
 
        
-
+@main.route('/view-repairs/<int:repair_id>')
+@login_required
+def view_repairs(repair_id):
+    repair = Repairs.query.get_or_404(task_id)
+    if not (current_user.is_admin):
+        if task.requested_by_id != current_user.id:
+            abort(403)
+    return render_template('main/repair_detail.html', repair=repair)
 
 
        
    
+@main.route('/new-requests')
+@login_required
+def view_new_requests():
+    
+    return render_template('main/new_requests.html')
 
+@main.route('/request-progress')
+@login_required
+def view_request_progress():
+    
+    return render_template('main/request_progress.html')
