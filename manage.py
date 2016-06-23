93adad7535dbdcef459eb4793c50b0f9b8be10c1
anne waithira <annewaithira28@gmail.com>
2016-06-23 18:37:12 +0300

    added app.run()

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ import os
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-from app.models import User
 from app import create_app, db
+
+
 
 app = create_app(os.getenv('MAINTRACKER_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -12,7 +13,7 @@ manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Facility=Facility, Repairs=Repairs, RepairPersons= RepairPersons)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
@@ -24,4 +25,4 @@ def deploy():
     pass
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
