1377deaf9ba5cac53169af46393818d89faaede0
anne waithira <annewaithira28@gmail.com>
2016-06-23 18:25:56 +0300

    added __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
-from flask_mail import Mail
-from flask_moment import Moment
+
+#from flask_mail import Mail
+#from flask_moment import Moment
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
